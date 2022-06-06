0b827ba8b2073d8d791f25c97fb08e7be340749d
Khiem19 <nghoangkhiem19@gmail.com>
2022-06-06 18:56:45 +0200

    new

--- Tic_tac_toe/test.cpp
+++ Tic_tac_toe/test.cpp
@@ -26,41 +26,85 @@ struct TicTacToe3 {
 		}
 	}
 
-	int win3(int s, int w, char marker) {
-		size = s;
+	int win3(std::vector<std::vector<char>> grid, int w, char marker) {
 		wincond = w;
 		int counterO,counterX =  0;
+
 		//wincond in rows
-		for (int i = 0; i < size-1; i++) {
-			if (grid[0][i] == 'O')
-			{
-				counterO++;
-			}
-			if (grid[0][i] == 'X')
-			{
-				counterX++;
-			}
-			if (grid[0][i] == 'O' && grid[0][i+1] == 'X')
-			{
-				counterO,counterX = 0;
-			}
-			if (counterO = wincond)
-			{
-				if (grid[i] == grid[i+1])
+		for (int i = 0; i < grid.size()-1; i++)
+		{
+			for (int j = 0; j < grid.size()-1; j++) {
+				if (grid[i][j] == 'O')
 				{
-					return 10;
-				}	
-			}
-			else if(counterX = wincond)
-			{
-				if (grid[i] == grid[i+1])
-				{
-					return -10;
+					counterO++;
 				}
-				
+				if (grid[i][j] == 'X')
+				{
+					counterX++;
+				}
+				if (grid[i][j] == 'O' && grid[i][j+1] == 'X')
+				{
+					counterO,counterX = 0;
+				}
+
+				if (counterO = wincond)
+				{
+					if (grid[i][j] == grid[i][j+1])
+					{
+						return 10;
+					}	
+				}
+				else if(counterX = wincond)
+				{
+					if (grid[i][j] == grid[i][j+1])
+					{
+						return -10;
+					}
+					
+				}
+				else return 0;
 			}
-			else return 1;
 		}
+
+		//wincond in column
+		for (int i = 0; i < grid.size()-1; i++)
+		{
+			for (int j = 0; j < grid.size()-1; j++) {
+				if (grid[i][j] == 'O')
+				{
+					counterO++;
+				}
+				if (grid[i][j] == 'X')
+				{
+					counterX++;
+				}
+				if (grid[i][j] == 'O' && grid[i+1][j] == 'X')
+				{
+					counterO,counterX = 0;
+				}
+
+				if (counterO = wincond)
+				{
+					if (grid[i][j] == grid[i][j])
+					{
+						return 10;
+					}	
+				}
+				else if(counterX = wincond)
+				{
+					if (grid[i][j] == grid[i+1][j])
+					{
+						return -10;
+					}
+					
+				}
+				else return 0;
+			}
+		}
+
+		//wincond for diagonals (%2=0 || %2 =1)
+
+		return 0;
 	}	
 
 	void player_move3(int s)
@@ -81,30 +125,36 @@ struct TicTacToe3 {
 		}
 	}
 
+
 	void play(int s) {
 		size = s;
 		int wincond = 3;
 		Init(size);
-		grid= {{'O','O','O'}};
-		print3(size);
+		// grid= {{'O','X','O','O','O'}};
+		// print3(size);
 	
-			if (win3(size,wincond,'O') == 10)
-			{
-				std::cout << "Player win !\n";
-				return;
-			}
-			else
-			{
-				std:: cout << "Computer win !\n";
-			}
+		// 	if (win3(grid,wincond,'O') == 10)
+		// 	{
+		// 		std::cout << "Player win !\n";
+		// 	}
+		// 	else
+		// 	{
+		// 		std:: cout << "Computer win !\n";
+		// 	}
+	
+
+
 	
 		// while (true) {
 		// 	player_move3(size);
 		// 	print3(size);
-		// 	if (win3(size,wincond))
+		// 	if (win3(size,wincond,'O'))
 		// 	{
 		// 		std::cout << "Player win !\n";
 		// 		return;
+		// 	}
+		// 	else{
+		// 		std::cout << "loser\n";
 		// 	}
 			
 		// }
@@ -138,6 +188,6 @@ struct TicTacToe3 {
 
 int main() {
 	TicTacToe3 game;
-	game.play(3);
+	game.play(5);
 	
 }
